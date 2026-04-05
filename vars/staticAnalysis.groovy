b855be4503360010bb721b6fc8867109c97bc11d
JohanyFlores <jfloresm1@student.obsbusiness.school>
2026-04-05 12:12:48 -0600

    Fix syntax: closing braces

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,31 +1,18 @@
 def call(Map config = [:]) {
-    // Parámetro manual del ejercicio 3
     def manualAbort = config.get('abortPipeline', false)
-    
-    // Variable de entorno para la rama (Ejercicio 4)
     def branchName = env.BRANCH_NAME ?: "unknown"
-    // Timeout de 5 minutos
-    timeout(time: 5, unit: 'MINUTES') { 
+
+    timeout(time: 5, unit: 'MINUTES') {
         script {
-            // Imprime el mensaje de ejecución
             sh 'echo "Ejecución de las pruebas de calidad de código"'
             echo "Rama actual detectada: ${branchName}"
 
-            // Implementación de la Heurística 
-            if (manualAbort == true) {
-                echo "Abortando: Parámetro manual 'abortPipeline' es True."
-                error "Análisis fallido (Corte manual)."
-            } 
-            else if (branchName == "master" || branchName == "main") {
+            if (manualAbort == true || branchName == "master" || branchName == "main" || branchName.startsWith("hotfix")) {
                 echo "Abortando: No se permite fallo en rama principal (${branchName})."
                 error "Análisis fallido en rama protegida."
-            } 
-            else if (branchName.startsWith("hotfix")) {
-                echo "Abortando: No se permite fallo en rama de parche crítico (${branchName})."
-                error "Análisis fallido en rama hotfix."
-            } 
-            else {
-                echo "Continuando: Rama '${branchName}' permitida para pruebas."
-        }
-    }
-}
+            } else {
+                echo "Continuando: Rama '${branchName}' permitida."
+            }
+        } 
+    } 
+} 

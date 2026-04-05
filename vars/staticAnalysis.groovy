63bab3134cb636b9109ce86ed0f9dacc822880ab
JohanyFlores <jfloresm1@student.obsbusiness.school>
2026-04-04 20:18:46 -0600

    Ejercicio 4: Implementación de heurística por ramas

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,22 +1,31 @@
-//staticAnalysis.groovy
-
 def call(Map config = [:]) {
-    def shouldAbort = config.abortPipeline ?: false
-
+    // Parámetro manual del ejercicio 3
+    def manualAbort = config.get('abortPipeline', false)
+    
+    // Variable de entorno para la rama (Ejercicio 4)
+    def branchName = env.BRANCH_NAME ?: "unknown"
     // Timeout de 5 minutos
-    timeout(time: 5, unit: 'MINUTES') {
-
+    timeout(time: 5, unit: 'MINUTES') { 
         script {
             // Imprime el mensaje de ejecución
             sh 'echo "Ejecución de las pruebas de calidad de código"'
-            
-            // Lógica de decisión
-            if (shouldAbort == true) {
-                echo "Quality Gate fallido. Abortando pipeline..."
-                error "Pipeline detenido por fallo en análisis estático."
-            } else {
-                echo "Análisis finalizado con éxito (o advertencias ignoradas). Continuando..."
-            }
+            echo "Rama actual detectada: ${branchName}"
+
+            // Implementación de la Heurística 
+            if (manualAbort == true) {
+                echo "Abortando: Parámetro manual 'abortPipeline' es True."
+                error "Análisis fallido (Corte manual)."
+            } 
+            else if (branchName == "master" || branchName == "main") {
+                echo "Abortando: No se permite fallo en rama principal (${branchName})."
+                error "Análisis fallido en rama protegida."
+            } 
+            else if (branchName.startsWith("hotfix")) {
+                echo "Abortando: No se permite fallo en rama de parche crítico (${branchName})."
+                error "Análisis fallido en rama hotfix."
+            } 
+            else {
+                echo "Continuando: Rama '${branchName}' permitida para pruebas."
         }
     }
 }

84d50c199977f0a0558449f81afc8a0694e38df4
JohanyFlores <jfloresm1@student.obsbusiness.school>
2026-04-05 16:48:38 -0600

    Configuración para QualityGate con sonarServer

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -4,19 +4,35 @@ def call(Map config = [:]) {
     
     // Variable de entorno para la rama (Ejercicio 4) 
     def branchName = env.BRANCH_NAME ?: "unknown"
+    def sonarServer = 'SonarQubeServer'
+
 
     // Timeout de 5 minutos
     timeout(time: 5, unit: 'MINUTES') { 
         script {
-            // Imprime el mensaje de ejecución
-            sh 'echo "Ejecución de las pruebas de calidad de código"'
+            withSonarQubeEnv(sonarServer) {
+                echo "Iniciando análisis de calidad con SonarScanner..."
+                sh "sonar-scanner -Dsonar.projectKey=practica_2_johany_flores -Dsonar.sources=."
+            }
+           
+            // Resultadod del QualityGate
+            def qg = waitForQualityGate()
+            echo "Resultado de SonarQube: ${qg.status}"
             echo "Rama actual detectada: ${branchName}"
+ 
 
             // Implementación de la Heurística
             if (manualAbort == true) {
                 echo "Abortando: Parámetro manual 'abortPipeline' es True."
                 error "Análisis fallido (Corte manual)."
             } 
+
+            //Condición para cuando el Quality Gate falle 
+            else if (qg.status != 'OK') {
+                echo "Abortando: El Quality Gate ha fallado con estado ${qg.status}."
+                error "Pipeline detenido por falta de calidad en el código."
+            }
+
             else if (branchName == "master" || branchName == "main") {
                 echo "Abortando: No se permite fallo en rama principal (${branchName})."
                 error "Análisis fallido en rama protegida."
